68c8e742e680a860a0096a9e1fafd09db2047c7e
Ritesh Kumar <157762698+Ritesh1244@users.noreply.github.com>
2024-10-08 01:40:19 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -47,7 +47,7 @@ function Login() {
         }
     
         try {
-            const response = await fetch('http://localhost:3000/auth/Login', {
+            const response = await fetch('https://deploy-auth-mern-api.vercel.app/auth/Login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(Logininfo),
